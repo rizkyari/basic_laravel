6d58abc3b6e9f0719236b48f1c78d4f7c9562a91
Rizky Ari <rizkyari>
2020-10-19 20:20:37 +0700

    adding about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
+    <title>About</title>
 </head>
 <body>
-    <h1>Welcome</h1>
-    <a href="/about">Go to about</a>
+    <h1>This is About Page</h1>
+    <a href="/">Back to home page</a>
 </body>
 </html>
