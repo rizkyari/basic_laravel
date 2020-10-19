c4189046752f6a9744f66be5ea767cacac73ac6e
Rizky Ari <rizkyari>
2020-10-19 21:13:27 +0700

    learn blade layout

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shows</title>
-</head>
-<body>
+@extends('layout.app')
+
+@section('content')
     <h1>Ini adalah artikel : {{$title}}</h1>
     <a href="/article">Back to article</a>
     <a href="/">Back to home</a>
-</body>
-</html>
+@endsection
