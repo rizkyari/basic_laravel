41377f8a643fa3e870d8f9ed7db8979cfaea0de3
Rizky Ari <rizkyari>
2020-10-20 10:59:15 +0700

    foreach blade

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,7 +8,13 @@ class ArticleController extends Controller
 {
     public function index()
     {
-        return view('article.article');
+        $articles = [
+            ['title'=>'judul pertama', 'subject'=>'ini isi pertama'],
+            ['title'=>'judul kedua', 'subject'=>'ini isi kedua'],
+            ['title'=>'judul ketiga', 'subject'=>'ini isi ketiga'],
+        ];
+
+        return view('article.article', compact('articles'));
     }
 
     public function show($slug)
--- resources/views/article/article.blade.php
+++ resources/views/article/article.blade.php
@@ -4,4 +4,10 @@
     <h1>This is Article Page</h1>
     <a href="/article/1">Go to first article</a>
     <a href="/">Back to home page</a>
+
+    @foreach ($articles as $article)
+        <p><strong>Judul : {{ $article['title'] }} </strong></p>
+        <p>Deskripsi : {{ $article['subject'] }} </p>
+    @endforeach
+
     @endsection
